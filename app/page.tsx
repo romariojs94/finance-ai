857b0b6be1985593987076cdd93f727766400856
romarijs94 <romariojdosantos@gmail.com>
2024-11-10 19:25:46 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./_components/ui/button";
 
 const page = () => {
   return (
-    <div>Ola, mundo</div>
-  )
-}
+    <div className="h-100-items-center flex min-h-screen items-center justify-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <Button className="text-3xl">Olá, mundo</Button>
+    </div>
+  );
+};
 
-export default page
+export default page;
